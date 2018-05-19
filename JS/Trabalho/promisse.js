3a6dde093f29da6bbd8d576a1839f24bb7dc88de
joao <joao_victor.dp@hotmail.com>
2018-05-19 16:31:13 -0300

    Aulas Pós Graduação WEB Dev ALFA 2018

--- JS/Trabalho/promisse.js
+++ JS/Trabalho/promisse.js
@@ -1,11 +1,17 @@
+//Atribuindo a variavel Formu o controle do formulario HTML
 let formu = document.getElementById("promisse");
 
 formu.addEventListener('submit', (e) => {
+	//Evitar Carregamento da Página
 	e.preventDefault();
 
+	//Recebendo valores do HTML
 	let gasolina = document.getElementById('gasolina');
 	let etanol = document.getElementById('etanol');
+	let fieldGasolina = document.getElementById('fieldGasolina');
+	let fieldEtanol = document.getElementById('fieldEtanol');
 
+	//Transformando os valores informados em Float
 	gas = parseFloat(gasolina.value);
 	et = parseFloat(etanol.value);
 	
@@ -17,13 +23,28 @@ formu.addEventListener('submit', (e) => {
 		window.alert('Valor inválido para etanol');
 	}else {
 
-		consumo = ( etanol / gasolina );
+	/*	consumo = ( etanol / gasolina );
 		if( consumo >= 0.7 ){
 			window.alert('Compensa Gasolina');
 		}else {
 			window.alert('Compensa Etanol');
 		}
+	*/
+
+		var promisse = new  Promise((resolve)=>{
+		    if( ( gas/et ) > 0.7) {
+		      fieldGasolina.style.background ="green";
+		      fieldEtanol.style.background ="white";
+		    }else{
+		      fieldEtanol.style.background ="green";
+		      fieldGasolina.style.background ="white";
+
+		    }
+		    resolve("Gasol.");
+	    });
 
 	}
 
 });
+
+
