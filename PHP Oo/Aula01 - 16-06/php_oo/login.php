cdad01c21dd9dc6ebdf1941dfb9062847c485ad1
joao <joao_victor.dp@hotmail.com>
2018-06-16 10:30:05 -0300

    Arquivos AUla de PHP OO

--- PHP Oo/Aula01 - 16-06/php_oo/login.php
+++ PHP Oo/Aula01 - 16-06/php_oo/login.php
@@ -3,10 +3,10 @@
 declare (strict_types = 1);
 
 //Realizando ligação do arquivo PHP com a classe Usuário
-require_once '../../class/Usuario.php';
+require_once '../../class/Admin.php';
 
 //Instanciando Novo Objeto
-$u = new Usuario("jao.victor.dp@gmail.com", "md5");
+$u = new Admin("jao.victor.dp@gmail.com", "md5");
 //$u->email = "jao.victor.dp@gmail.com";
 //$u->senha = "md5";
 
