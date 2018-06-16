7f0cabf3939fb514417f452e77007aacb654f091
joao <joao_victor.dp@hotmail.com>
2018-06-16 13:37:46 -0300

    Arquivos AUla de PHP OO

--- PHP Oo/Aula01 - 16-06/php_oo/pdo.php
+++ PHP Oo/Aula01 - 16-06/php_oo/pdo.php
@@ -4,3 +4,12 @@
 $dsn = 'mysql:host=localhost;dbname=mysql';
 $dbh = new PDO($dsn, 'root', '');
 
+$sql = 'SHOW TABLES';
+
+$stmt = $dbh->prepare($sql);
+$stmt ->execute();
+
+while( $record = $stmt->fetchObject() ){
+    echo $record->Tables_in_mysql . "<br>";
+}
+$dbh = NULL;
